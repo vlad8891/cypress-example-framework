ad3e8e214c2621f6f12433c5d55539ef1a3eb68a
unknown <vlad.opris88@gmail.com>
2022-07-03 19:59:25 +0300

    Updated package details

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://automationpractice.com",
+    defaultCommandTimeout: 10000,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    numTestsKeptInMemory: 0,
+  },
+  env: {
+    apiUrl: "https://dummy.restapiexample.com/api/v1",
+  },
+});
